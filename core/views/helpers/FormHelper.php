2ddb5be24edcc8b49f7e4aa163f0bc980a54d51b
Adam Morrison <marc0adam@hotmail.com>
2014-04-29 20:25:05 -0400

    Enhance FormHelper
    Add select() and textarea()

--- core/views/helpers/FormHelper.php
+++ core/views/helpers/FormHelper.php
@@ -40,10 +40,76 @@ class FormHelper extends ModHelper {
 		$this->text($dataName, $params);
 	}
 	
-	function select() {
+	/*
+	* @param array $params The HTML attributes for the select element.
+	*		Additional $params:
+	*			'empty' => false: Do not add an empty <option>
+	*			'empty' => true: Add an empty <option> with empty value and text
+	*			'empty' => string: Use the string as the text for empty value <option>
+	*/
+	function select($dataName, $choices = array(), $selected = NULL, $params = array()) {
+		$modelName = '0';
+		$fieldName = $dataName;
+		if (strpos($dataName, '.') > 0) {
+			list($modelName, $fieldName) = explode('.', $dataName, 2);
+		} else {
+			$params['name'] = $dataName;
+		}
 		
+		$params['id'] = PockyApp::camelCase(str_replace('.', '_', $dataName));
+		if (!isset($params['name'])) {
+			$params['name'] = 'data['. $modelName. ']['. $fieldName. ']';
+		}
+		if ($selected == NULL) {
+			if (isset($this->view->data[$modelName][$fieldName])) {
+				$selected = $this->view->data[$modelName][$fieldName];
+			}
+		}
+		$opts = array();
+		if (isset($params['empty'])) {
+			if (is_string($params['empty'])) {
+				$opts[] = '<option value="">'. $params['empty']. '</option>';
+			} elseif ($params['empty'] == true) {
+				$opts[] = '<option value=""></option>';
+			} else { // 'empty' == false
+				//do nothing
+			}
+			unset($params['empty']);
+		}
+		foreach($choices as $value => $text) {
+			$opts[] = '<option value="'. $value. '">'. $text. '</option>';
+		}
+		
+		echo '<select ';
+		foreach($params as $key => $value) {
+			echo $key. '="'. addslashes($value). '" ';
+		}
+		echo ">\n";
+		echo implode("\n", $opts);
+		echo "</select>\n";
 	}
 	
 	
+	function textarea($dataName, array $params = array()) {
+		$modelName = '0';
+		$fieldName = $dataName;
+		if (strpos($dataName, '.') > 0) {
+			list($modelName, $fieldName) = explode('.', $dataName, 2);
+		} else {
+			$params['name'] = $dataName;
+		}
+		
+		$params['id'] = PockyApp::camelCase(str_replace('.', '_', $dataName));
+		if (!isset($params['name'])) {
+			$params['name'] = 'data['. $modelName. ']['. $fieldName. ']';
+		}
+		$value = (isset($this->view->data[$modelName][$fieldName])) ? $this->view->data[$modelName][$fieldName] : '';
+		
+		echo '<textarea ';
+		foreach($params as $key => $value) {
+			echo $key. '="'. addslashes($value). '" ';
+		}
+		echo '>'. $value. '</textarea>';
+	}
 	
 }
